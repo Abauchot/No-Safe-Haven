9838e56239f2a18429bc388f0fa5b8fb3aa75b99
Antoine Bauchot <bchtantoine@gmail.com>
2025-03-10 16:23:23 +0100

    Animatior - Animation

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,6 +4,7 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D _rb;
     private PlayerLocomotionInput _playerLocomotionInput;
+    private Animator _animatior;
     
     [SerializeField] public float speed = 5f;
     
@@ -12,16 +13,30 @@ public class PlayerController : MonoBehaviour
     {
         _rb = GetComponent<Rigidbody2D>();
         _playerLocomotionInput = GetComponent<PlayerLocomotionInput>();
+        _animatior = GetComponent<Animator>();
     }
 
     private void FixedUpdate()
     {
         MovePlayer();
+        UpdateAnimation();
     }
 
     private void MovePlayer()
     {
         Vector2 movement = _playerLocomotionInput.MovementInput;
         _rb.linearVelocity = movement * speed;
+        
+    }
+    
+    private void UpdateAnimation()
+    {
+        Vector2 movement = _playerLocomotionInput.MovementInput;
+        _animatior.SetFloat("Speed_down_f", movement.y <  0 ? Mathf.Abs(movement.y) : 0);
+        Debug.Log(_animatior.GetCurrentAnimatorStateInfo(0).normalizedTime);
+        _animatior.SetFloat("Speed_up_f", movement.y >  0 ? movement.y : 0);
+        Debug.Log(_animatior.GetCurrentAnimatorStateInfo(0).normalizedTime);
+        _animatior.SetFloat("Speed_right_f", Mathf.Abs(movement.x));
+        Debug.Log(_animatior.GetCurrentAnimatorStateInfo(0).normalizedTime);
     }
 }
